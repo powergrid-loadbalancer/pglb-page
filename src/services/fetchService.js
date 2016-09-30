dc9193ed8d77f65fbd0afb0c5dba13a0ec0db8db
LeanderK <Leander.Kurscheidt@gmx.de>
2016-09-30 09:47:07 +0200

    fixed fetchService

--- src/services/fetchService.js
+++ src/services/fetchService.js
@@ -9,6 +9,7 @@ let counter = 0
 
 export function fetchEntries () {
     let entryStates = EntriesStore.getEntryStates()
+    console.log(entryStates)
 
     return fetch(ENV_VARS.SERVER_URL + "/head", {
         method: "GET",
@@ -19,8 +20,8 @@ export function fetchEntries () {
         .then(response => {
             if (response.status === 200) {
                 response.json().then(jsonArray => {
-
-                    let entries = Immutable.List.of(jsonArray)
+                    console.log("1")
+                    let entries = Immutable.List(jsonArray)
                         .filter(meter => meter.Meter !== null)
                         .map(meter => {
                             var rawMeterID = parseInt(meter.Meter, 10);
@@ -33,14 +34,16 @@ export function fetchEntries () {
                             return meter
                         })
                         .groupBy(meter => meter.groupID)
-                        .map((groupId, meters) => {
+                        .map((meters, groupId) => {
                             let checked = false
 
                             if (entryStates.length >= (ENV_VARS.CONSTANTS.MAX_SIZE)) {
                                 checked = entryStates[groupId].checked
                             }
-                            let normalValueAkk = meters.reduce((akk, meter) => akk + meter.normalValue ,0)
-                            let buyingValueAkk = meters.reduce((akk, meter) => akk + meter.buyingValue ,0)
+                            let normalValueAkk = meters.reduce((akk, meter) => akk + parseFloat(meter.normalValue) ,0.0)
+                            let buyingValueAkk = meters.reduce((akk, meter) => akk + parseFloat(meter.buyingValue) ,0.0)
+
+                            console.log(normalValueAkk, buyingValueAkk)
                             return {
                                 type: ENV_VARS.CONSTANTS.CONSUMER,
                                 meterId: groupId,
@@ -49,17 +52,29 @@ export function fetchEntries () {
                                 checked: checked
                             }
                         })
-                        .reduce((array, meter, groupId) => array[groupId] = meter, [])
+                        .reduce((array, meter, groupId) => {
+                            array[groupId] = meter
+                            return array
+                        }, new Array())
 
                     var producer = jsonArray.find(meter => meter.Meter === null);
+
+                    let checked = false
+
+                    if (entryStates.length >= (ENV_VARS.CONSTANTS.MAX_SIZE)) {
+                        checked = entryStates[ENV_VARS.CONSTANTS.MAX_SIZE - 1].checked
+                    }
 
                     entries[ENV_VARS.CONSTANTS.MAX_SIZE - 1] = {
                         type: "PRODUCER",
                         meterId: ENV_VARS.CONSTANTS.MAX_SIZE - 1,
-                        normalValue: producer.normalValue,
-                        buyingValue: producer.buyingValue,
-                        checked: entryStates[ENV_VARS.CONSTANTS.MAX_SIZE - 1].checked
+                        normalValue: parseFloat(producer.normalValue),
+                        buyingValue: parseFloat(producer.buyingValue),
+                        checked: checked
                     }
+
+                    console.log("2")
+                    console.log(entries)
 
                     setEntries(entries)
                 })
